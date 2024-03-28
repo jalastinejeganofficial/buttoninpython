a2c06a06ef14a942e87ba130769612c38590fd71
Jalastine Jegan <103115358+jalastinejeganofficial@users.noreply.github.com>
2024-03-28 12:57:44 +0530

    Create a button index.py

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 from tkinter import *
-
 root=Tk()
 root.title("Simple Calculator JJ")
-button = Button(root, text="Click me!",width=200)
-button.pack()
+button = Button(root, text="Click me!", width=200)
+#To Display a Button using two Ways
+button.pack() #pack use to Display the button in center.
+button.grid(row=0, column=0) #grid use to set row and column to where is Display
